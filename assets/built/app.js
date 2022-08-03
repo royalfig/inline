6add7cb809fe6e72fe73eb71bc7be45598118d0f
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2022-08-02 23:03:04 -0400

    wip

--- assets/built/app.js
+++ assets/built/app.js
@@ -5,8 +5,9 @@
   // eslint-disable-next-line no-undef
   {
     const script = document.createElement('script');
-    script.src = `http://${(window.location.host || 'localhost').split(':')[0]
-    }:35729/livereload.js?snipver=1`;
+    script.src = `http://${
+    (window.location.host || 'localhost').split(':')[0]
+  }:35729/livereload.js?snipver=1`;
     document.head.append(script);
     console.info('Reload script added');
   }
@@ -18,14 +19,23 @@
       navigator.clipboard.writeText(window.location.href);
       target.closest('.i-copy-button').classList.toggle('copied');
       setTimeout(
-  () =>
-        target.closest('.i-copy-button').classList.toggle('copied'),
-         2000,
-  );
+        () => target.closest('.i-copy-button').classList.toggle('copied'),
+        2000,
+      );
     }
 
-    if (target.closest('.i-menu')) {
-      console.log('menu');
+    if (target.closest('.i-menu-button')) {
+      document.querySelector('.i-menu').classList.toggle('i-menu-show');
+      document.body.classList.toggle('no-scroll');
+    }
+
+    if (target.closest('.i-post-header-caption')) {
+      target.closest('.i-post-header-caption').classList.toggle('i-caption-show');
+    }
+
+    if (target.closest('.i-menu-close')) {
+      document.querySelector('.i-menu').classList.toggle('i-menu-show');
+      document.body.classList.toggle('no-scroll');
     }
   }
 
--- src/js/app/index.js
+++ src/js/app/index.js
@@ -4,8 +4,9 @@ import '../../css/app.css';
 // eslint-disable-next-line no-undef
 if (ENV === 'development') {
   const script = document.createElement('script');
-  script.src = `http://${(window.location.host || 'localhost').split(':')[0]
-    }:35729/livereload.js?snipver=1`;
+  script.src = `http://${
+    (window.location.host || 'localhost').split(':')[0]
+  }:35729/livereload.js?snipver=1`;
   document.head.append(script);
   console.info('Reload script added');
 }
@@ -17,14 +18,23 @@ function clickHandler(e) {
     navigator.clipboard.writeText(window.location.href);
     target.closest('.i-copy-button').classList.toggle('copied');
     setTimeout(
-() =>
-      target.closest('.i-copy-button').classList.toggle('copied'),
-       2000,
-);
+      () => target.closest('.i-copy-button').classList.toggle('copied'),
+      2000,
+    );
   }
 
-  if (target.closest('.i-menu')) {
-    console.log('menu')
+  if (target.closest('.i-menu-button')) {
+    document.querySelector('.i-menu').classList.toggle('i-menu-show');
+    document.body.classList.toggle('no-scroll');
+  }
+
+  if (target.closest('.i-post-header-caption')) {
+    target.closest('.i-post-header-caption').classList.toggle('i-caption-show');
+  }
+
+  if (target.closest('.i-menu-close')) {
+    document.querySelector('.i-menu').classList.toggle('i-menu-show');
+    document.body.classList.toggle('no-scroll');
   }
 }
 
